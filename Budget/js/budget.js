d66859cd93d38a6d60be4984c077f2bcb4ca7629
robpants2 <34636979+robpants2@users.noreply.github.com>
2017-12-18 08:18:38 -0500

    Add files via upload

--- Budget/js/budget.js
+++ Budget/js/budget.js
@@ -1,5 +1,39 @@
 
 /*-- GLOBAL VARIABLES -----------------------------------------------------------------------------*/
+var controls = [
+	{
+		name: "Control 1",
+		icon: "icon"
+	},
+	{
+		name: "Control 2",
+		icon: "icon"
+	}
+];
+
+var accounts = [
+	{
+		name: "Account 1",
+		balance: 200,
+		expenses: []
+	},
+	{
+		name: "Account 2",
+		balance: 300,
+		expenses: []
+	}
+];
+
+var categories = [
+	{
+		name: "Category 1",
+		budget: 150
+	},
+	{
+		name: "Category 2",
+		budget: 200
+	}
+];
 
 
 /*-- ON PAGE LOAD --------------------------------------------------------------------------*/
